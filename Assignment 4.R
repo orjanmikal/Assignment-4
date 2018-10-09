8c6b19a33d3d4cc3124fb6a819e19b53d172700a
orjanmikal <orjanantonsen@hotmail.com>
2018-10-09 11:30:44 +0200

    Tredje commit

--- Assignment 4.R
+++ Assignment 4.R
@@ -1,13 +1,13 @@
 rm(list=ls())
 
-#Laster pakker
+#loading packages
 library(rvest)
 library(tibble)
 library(dplyr)
 library(tidyr)
 library(tidyverse)
 
-#henter ned nettsiden
+#Getting the website
 url <- "https://w2.brreg.no/kunngjoring/kombisok.jsp?datoFra=01.01.2018&datoTil=03.10.2018&id_region=100&id_fylke=19&id_kommune=-+-+-&id_niva1=51&id_niva2=-+-+-&id_bransje1=0"
 
 webpage <- read_html(url)
